ddb82a5ef8651fd6cc0c4971316e299eac2a45e4
Vaibhav Singh <singhvaibhav715@gmail.com>
2022-03-10 10:52:10 +0530

    Added Changes

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 import * as THREE from "three"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+new OrbitControls( camera, renderer.domElement );
+camera.position.z = 5;
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  
+  }
+
+  
+  //For Animation Objects
+function animate() {
+    requestAnimationFrame(animate);
+    onWindowResize()
+    renderer.render(scene, camera);
+  }
+  
+  animate()
