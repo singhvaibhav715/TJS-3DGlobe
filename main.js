fdcc9c4c909c1f49894bb409d63e2026bdc731af
Vaibhav Singh <singhvaibhav715@gmail.com>
2022-03-10 16:50:40 +0530

    Added Changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from "three"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { TextureLoader } from "three";
 
 
 
@@ -14,14 +15,26 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.antialias=true
+renderer.setPixelRatio=window.devicePixelRatio
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 new OrbitControls( camera, renderer.domElement );
-camera.position.z = 5;
 
+camera.position.z = 10;
 
-const 
+const textureLoader =new TextureLoader()
+const matcap= textureLoader.load('images/globe_texture.jpg')
+
+const sphereGeometry=new THREE.SphereGeometry(5,50,50)
+console.log(sphereGeometry);
+const sphereMaterial = new THREE.MeshBasicMaterial()
+sphereMaterial.map=matcap
+const sphere =new THREE.Mesh(sphereGeometry,sphereMaterial)
+sphereGeometry.center()
+scene.add(sphere)
+
 
 function onWindowResize(){
 
@@ -37,6 +50,8 @@ function onWindowResize(){
 function animate() {
     requestAnimationFrame(animate);
     onWindowResize()
+    sphere.rotation.x+=0.001
+    sphere.rotation.y+=0.001
     renderer.render(scene, camera);
   }
   
